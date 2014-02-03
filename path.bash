8eb735ee7c1d788da976d57ceaade30f7f062f0f
Dieze <dieze51@gmail.com>
2014-02-03 10:35:13 +0100

    Maj path et prompt, maintenant avec git-prompt et vagrant status

--- path.bash
+++ path.bash
@@ -1,8 +1,20 @@
-#export PATH="/Applications/ConTeXt/tex/texmf-osx-64/bin:$PATH"
+# unix
+export PATH="/bin:$PATH"
+export PATH="/sbin:$PATH"
+export PATH="/usr/bin:$PATH"
+export PATH="/usr/sbin:$PATH"
+export PATH="/usr/local/bin:$PATH"
+export PATH="/opt/local/bin:$PATH"
+export PATH="/opt/local/sbin:$PATH"
+
+# system-wide
+export PATH="/usr/local/mysql/bin:$PATH"
+
+# user-specific
+export PATH="/Users/adrien/devtools/autotools-bin/bin:$PATH"
+
+# applications
 export PATH="/Applications/CMake.app/Contents/bin:$PATH"
+export PATH="/Applications/ConTeXt/tex/texmf-osx-64/bin:$PATH"
 export PATH="/Applications/Doxygen.app/Contents/Resources:$PATH"
 export PATH="/Applications/Skim.app/Contents/MacOS:$PATH"
-export PATH="/Users/adrien/Developer/bin:$PATH"
-export PATH="/opt/local/bin:/opt/local/sbin:$PATH"
-export PATH="/usr/local/bin:$PATH"
-export PATH="/usr/local/mysql/bin:$PATH"
