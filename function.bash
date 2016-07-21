aa8ac6dfe1333bcc0ca57239215d3e31f99e722b
Adrien Desfourneaux <adrien.desfourneaux@gmail.com>
2016-07-21 10:50:36 +0200

    Add function branches
    Allow to list git branches with their description.

--- function.bash
+++ function.bash
@@ -20,3 +20,14 @@ function force() {
 chrome_app() {
     open -na Google\ Chrome --args --user-data-dir=$1 --app=$2 > /dev/null 2>&1
 }
+
+# list git branches with their descriptions
+function branches() {
+    branch=""
+    branches=`git branch --list`
+    while read -r branch; do
+    clean_branch_name=${branch//\*\ /}
+    description=`git config branch.$clean_branch_name.description`
+    printf "%-15s %s\n" "$branch" "$description"
+    done <<< "$branches"
+}
