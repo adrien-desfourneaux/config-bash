bb0fa0224dabd6a896b61f84a7c18a8e6900cdd3
Adrien Desfourneaux <adrien.desfourneaux@gmail.com>
2016-10-20 23:22:38 +0200

    Add alias git-review to review commits

--- alias.bash
+++ alias.bash
@@ -1,3 +1,4 @@
 alias ls='ls -FG' 
 alias tree="find . -print | sed -e 's;[^/]*/;|____;g;s;____|; |;g'"
+alias git-review='for hash in $(git log HEAD..FETCH_HEAD --reverse --pretty=%H); do git show $hash --pretty=medium --color=always | less -R; done'
 eval "$(hub alias -s)"
