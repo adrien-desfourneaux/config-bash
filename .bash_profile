eca4ac39eaba988ae0eeb16d1dd55e1866335b1d
Adrien Desfourneaux <adrien.desfourneaux@gmail.com>
2016-10-20 23:24:50 +0200

    Add git commands hub completion for bash

--- .bash_profile
+++ .bash_profile
@@ -1,4 +1,5 @@
 source "$HOME/.bash/alias.bash"
+source "$HOME/.bash/completion.bash"
 source "$HOME/.bash/function.bash"
 source "$HOME/.bash/path.bash"
 source "$HOME/.bash/prompt.bash"
---
+++ completion.bash
@@ -0,0 +1,6 @@
+# hub repository contains tab-completion scripts for bash and zsh.
+# These scripts complement existing completion scripts that ship with git.
+
+if [ -f /usr/local/etc/bash_completion.d/hub.bash_completion.sh ]; then
+    . /usr/local/etc/bash_completion.d/hub.bash_completion.sh
+fi
